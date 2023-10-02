a15de6f63f2a8ecef9e22c1e7e022799f73d4552
satishn92 <nvrn.satish@gmail.com>
2023-10-03 00:34:49 +0530

    Added profile page to get api key

---
+++ src/components/nativeAuthClient/native.auth.client.ts
@@ -0,0 +1,88 @@
+import axios from "axios";
+import { NativeAuthClientConfig } from "./entities/native.auth.client.config";
+
+export class NativeAuthClient {
+  private readonly config: NativeAuthClientConfig;
+
+  constructor(config?: Partial<NativeAuthClientConfig>) {
+    this.config = Object.assign(new NativeAuthClientConfig(), config);
+  }
+
+  getToken(address: string, token: string, signature: string): string {
+    const encodedAddress = this.encodeValue(address);
+    const encodedToken = this.encodeValue(token);
+
+    const accessToken = `${encodedAddress}.${encodedToken}.${signature}`;
+    return accessToken;
+  }
+
+  async initialize(extraInfo: any = {}): Promise<string> {
+    const blockHash = await this.getCurrentBlockHash();
+    const encodedExtraInfo = this.encodeValue(JSON.stringify(extraInfo));
+    const origin = this.encodeValue(this.config.origin);
+
+    return `${origin}.${blockHash}.${this.config.expirySeconds}.${encodedExtraInfo}`;
+  }
+
+  async getCurrentBlockHash(): Promise<string> {
+    if (this.config.gatewayUrl) {
+      return await this.getCurrentBlockHashWithGateway();
+    }
+    return await this.getCurrentBlockHashWithApi();
+  }
+
+  private async getCurrentBlockHashWithGateway(): Promise<string> {
+    const round = await this.getCurrentRound();
+    const url = `${this.config.gatewayUrl}/blocks/by-round/${round}`;
+    const response = await this.get(url);
+    const blocks = response.data.data.blocks;
+    const block = blocks.filter((block: { shard: number }) => block.shard === this.config.blockHashShard)[0];
+    return block.hash;
+  }
+
+  private async getCurrentRound(): Promise<number> {
+    if (!this.config.gatewayUrl) {
+      throw new Error("Gateway URL not set");
+    }
+    if (!this.config.blockHashShard) {
+      throw new Error("Blockhash shard not set");
+    }
+
+    const url = `${this.config.gatewayUrl}/network/status/${this.config.blockHashShard}`;
+    const response = await this.get(url);
+    const status = response.data.data.status;
+    return status.erd_current_round;
+  }
+
+  private async getCurrentBlockHashWithApi(): Promise<string> {
+    try {
+      const url = `${this.config.apiUrl}/blocks/latest?ttl=${this.config.expirySeconds}&fields=hash`;
+      const response = await this.get(url);
+      if (response.data[0].hash !== undefined) {
+        return response.data[0].hash;
+      }
+    } catch (error) {}
+    return this.getCurrentBlockHashWithApiFallback();
+  }
+
+  private async getCurrentBlockHashWithApiFallback(): Promise<string> {
+    let url = `${this.config.apiUrl}/blocks?size=1&fields=hash`;
+    if (this.config.blockHashShard !== undefined) {
+      url += `&shard=${this.config.blockHashShard}`;
+    }
+
+    const response = await this.get(url);
+    return response.data[0].hash;
+  }
+  encodeValue(str: string) {
+    return this.escape(Buffer.from(str, "utf8").toString("base64"));
+  }
+
+  private escape(str: string) {
+    return str.replace(/\+/g, "-").replace(/\//g, "_").replace(/=/g, "");
+  }
+
+  private async get(url: string): Promise<any> {
+    return await axios.get(url, { headers: this.config.extraRequestHeaders });
+  }
+}
