0f3071ccdbb72b113e8f066fd6f3132b1d812435
satishn92 <nvrn.satish@gmail.com>
2023-10-13 23:54:01 +0530

    Removes the apiKey from profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,36 +1,13 @@
 'use client';
 
 import { useGetAccount, useGetLoginInfo } from '@multiversx/sdk-dapp/hooks';
-import { useState } from 'react';
-import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 export default function Profile() {
   const { address } = useGetAccount();
   const { tokenLogin } = useGetLoginInfo();
-  const [apiKey, setApiKey] = useState('');
-  const [loading, setLoding] = useState(false);
 
-  const getApiKey = async () => {
-    try {
-      setLoding(true);
-      if (tokenLogin?.nativeAuthToken) {
-        const apiKeyResponse = await axios.get(
-          'https://2rkm8gkhk7.execute-api.eu-central-1.amazonaws.com/Prod/apiKey',
-          {
-            headers: {
-              Authorization: tokenLogin.nativeAuthToken
-            }
-          }
-        );
-        setApiKey(apiKeyResponse?.data?.apiKey);
-      }
-      setLoding(false);
-    } catch (error) {
-      console.log('error', error);
-      setLoding(false);
-    }
+  const copyToClipBoard = async () => {
+    navigator.clipboard.writeText(tokenLogin?.nativeAuthToken || '');
   };
 
   return (
@@ -40,18 +17,13 @@ export default function Profile() {
         <p className='info-value'>{address}</p>
       </div>
       <div className='info-container'>
-        <p className='label'>API Key</p>
-        <p className='info-value'>{apiKey}</p>
+        <p className='label'>Native Auth Token</p>
+        <p className='info-value'>{tokenLogin?.nativeAuthToken || ''}</p>
         <button
-          disabled={loading}
           className='bg-gray-300 p-2.5 rounded min-w-[100px]'
-          onClick={getApiKey}
+          onClick={copyToClipBoard}
         >
-          {loading ? (
-            <FontAwesomeIcon icon={faSpinner} className='text-muted fa-spin-pulse' />
-          ) : (
-            'Get Api Key'
-          )}
+          Copy to the ClipBoard
         </button>
       </div>
     </div>
