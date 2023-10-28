f0a26c920c8e27d7afa2336ed3451685e0966ed4
jnschilling <jeannoel.schilling@gmail.com>
2023-10-28 19:29:45 +0200

    video title add

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -53,7 +53,7 @@ export default function Profile() {
           <iframe 
             width="560" height="315" 
             src="https://www.youtube.com/embed/wy_onMYQn3Q?si=YrOV5mrSOtPagLoW" 
-            title="YouTube video player" 
+            title="Locki Demo #1" 
             frameborder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
             allowfullscreen
