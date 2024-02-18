31fd82cfbcaecf084246b72b10c42665ecd4caf3
satishn92 <nvrn.satish@gmail.com>
2024-02-19 00:24:54 +0530

    Loads python script in trade data page

--- src/app/tradedata/page.tsx
+++ src/app/tradedata/page.tsx
@@ -4,12 +4,18 @@ import { useDataNftMint } from '@/hooks';
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useGetAccountInfo } from '@multiversx/sdk-dapp/hooks';
+import { useSearchParams } from 'next/navigation';
 import { useState } from 'react';
 
 export default function Tradedata() {
   const [isMinting, setIsMinting] = useState(false);
+  const searchParams = useSearchParams();
   const accountInfo = useGetAccountInfo();
   const { mint } = useDataNftMint(accountInfo?.address);
+
+  const encodedDataScript = searchParams?.get('script') || '';
+  const dataScript = atob(encodedDataScript);
+
   const handleMintBtnClick = async () => {
     setIsMinting(true);
     await mint(
@@ -25,6 +31,12 @@ export default function Tradedata() {
   return (
     <div className='flex h-screen'>
       <div className='flex-1 flex flex-col'>
+      <div
+          className='flex-1 bg-code bg-cover text-gray-200 max-h-full p-10 border-b border-gray-600 relative overflow-y-auto'
+          style={{ whiteSpace: 'pre-line' }}
+        >
+          {dataScript}
+        </div>
         <div className='absolute top-0 left-1/2 transform -translate-x-1/2 '>
           <button
             className='bg-blue-200 text-blue-600 p-2.5 rounded min-w-[150px] mt-5 mr-5'
