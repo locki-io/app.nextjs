8fe7288eca1ce48f636fa940cb9fa7fa5219bfe2
jnschilling <jeannoel.schilling@gmail.com>
2024-03-11 08:53:34 +0100

    increase fontsize and gpt token adjustment

--- src/components/Chat/ChatHeader.tsx
+++ src/components/Chat/ChatHeader.tsx
@@ -6,7 +6,7 @@ const ChatHeader: FC = () => {
       <p className='text-xs text-zinc-800 mb-1'>Chat with </p>
       <div className='flex gap-1.5 items-center'>
         <p className='w-2 h-2 rounded-full bg-green-500' />
-        <p className='font-medium text-sm'>Locki companion</p>
+        <p className='font-medium text-xl'>Locki companion</p>
       </div>
     </>
   );
